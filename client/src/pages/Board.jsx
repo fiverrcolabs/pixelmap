7d0ceac0ba9c1eabcddf72886472afab1d90fce4
sanduninduwara <sanduninduwara2@gmail.com>
2022-09-20 10:34:40 +0530

    add zoom in zoom out

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,131 +1,187 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 
 function Board() {
 
     let board = []
-    for (let i = 1; i < 101; i++) {
-        board.push({
-            id: i,
-            state: "free"
-        });
+    for (let i = 1; i < 10001; i++) {
+      board.push({
+        id: i,
+        state: 'free',
+        color: "#fffff"
+      });
     }
     console.log(board)
-
+  
     // useEffect(() => {
     //     // let board = []
-    //     for (let i = 1; i < 26; i++) {
-    //         board.push({
-    //             id:i,
-    //             state:"free"
-    //         });
-    //     }
-    //     console.log(board)
+    //     // return ()=>{
+    //         for (let i = 1; i < 101; i++) {
+    //             board.push({
+    //                 id: i,
+    //                 state: "free",
+    //                 color:"#fffff"
+    //             });
+    //         }
+    //         console.log(board)
+    //     // }
+  
     // }, []);
-
-
+  
+  
     const [currentClick, setCurrentClick] = useState(0);
     const [color, setColor] = useState('#1f80ff');
     const [credit, setCredit] = useState(2);
-
-
-
-
+    // console.log(board)
+  
+  
+  
+  
     const onclick = (e) => {
-
-        console.log(e.target.id)
-        // e.target.classList.add("clicked")
+  
+      console.log(e.target.id)
+      // e.target.classList.add("clicked")
+      if(board[e.target.id-1].state==='free'){
         setCurrentClick(e.target.id)
-        
+      }else{
+        console.log("occupied")
+      }
+      
+      
+  
     }
     const onSubmit = () => {
-
-        console.log("submit")
-        if(currentClick){
-            let el = document.getElementById(currentClick);
-            el.style.backgroundColor = color;
-            setCredit(credit-1)
-            //todo save in database
-        }
-       
-        
+  
+      console.log("submit")
+      if (currentClick) {
+        let el = document.getElementById(currentClick);
+        el.style.backgroundColor = color;
+  
+        //    console.log(board[parseInt(currentClick)-1].color ,color)
+        //    board[parseInt(currentClick)-1].color=String(color)
+        console.log(board)
+  
+        setCredit(credit - 1)
+  
+        //todo save in database
+      }
+  
+  
     }
-
+  
     const onChange = (e) => {
-
-        console.log(e.target.value)
-        setColor(e.target.value)
-         
+  
+      console.log(e.target.value)
+      setColor(e.target.value)
+  
     }
-
-    const pathMatchRoute = (id) => {
-     
-        if ( parseInt(id) ===  parseInt(currentClick)) {
+  
+    const pathMatchRoute = (num) => {
+  
+    if(num.state==='free'){
+        if (parseInt(num.id) === parseInt(currentClick)) {
             return true
-        }
-        else{
+          }
+          else {
             return false
-        }
+          }
     }
-
+     
+    }
+  
     return (
-        <>
-            <div className='bcontainer' >
-
-                
-            <div className='row d-flex justify-content-center m-3 p-3 ' >
-                        <input type="button" className='btn btn-danger' value="Logout"  />
-
-                    </div>
-
-                <div className="grid-container">
-
-                    {board.map((num) => (
-                        <div className={pathMatchRoute(num.id)?"grid-item block clicked" :"grid-item block"}
-                           
-                        id={num.id} onClick={onclick} key={num.id}>{num.id}</div>
-                    ))
-                    }
-
-                </div>
-
-
-                <div className='row mt-3' >
-                    <div className='col  d-flex justify-content-center m-3 p-1 border'>
-                        <label for="colorpicker">Color Picker:</label>
-                        <input type="color" id="colorpicker" value={color} onChange={onChange} className='colorpicker'/>
-                        {/* <div class="input-group">
-                            <span class="input-group-text">Color Picker</span>
-                            <input type="color" class="form-control" id="colorpicker" value="#0000ff"/>
-                            
-                        </div> */}
-
-                    </div>
-
-                    <div className='col  d-flex justify-content-center m-3 p-1 border'>
-                        <button type='subbit' className='btn btn-primary' onClick={onSubmit} disabled={!credit} > confirm</button>
-
-                    </div>
-
-                    <div className='col d-flex justify-content-center m-3 p-1 border' >
-                        
-                        <label for="credits">available pixels:</label>
-                        <input type="button" id="credits" className='btn btn-outline-primary' value={credit} disabled />
-                        {/* <p  value={credit}>{credit}</p> */}
-
-                    </div>
-
-
-                </div>
-
-
-
-
-
+  
+      <>
+        <div className='bcontainer' >
+  
+  
+  
+  
+        <div className='row d-flex justify-content-center m-3 p-3 ' >
+          <input type="button" className='btn btn-danger' value="Logout" />
+  
+        </div>
+  
+  
+        {/* <div style={{height: 510, width: 510}}> */}
+  
+        <TransformWrapper
+          defaultScale={1}
+          // options={transformOptions}
+          defaultPositionX={1}
+          defaultPositionY={1}
+        >
+          <TransformComponent>
+  
+            {/* <div className='bcontainer' > */}
+  
+  
+  
+  
+            <div className="grid-container">
+  
+              {board.map((num) => (
+                <div className={pathMatchRoute(num) ? "grid-item block clicked" : "grid-item block"}
+                  style={ {
+                    backgroundColor: num.color
+                  }}
+                  id={num.id} onClick={onclick} key={num.id}  >{num.id}</div>
+              ))}
+  
             </div>
-
-
-        </>
+  
+  
+  
+  
+  
+  
+  
+  
+            {/* </div> */}
+  
+  
+  
+  
+          </TransformComponent>
+  
+        </TransformWrapper>
+        
+        
+        <div className='row mt-3'>
+          <div className='col  d-flex justify-content-center m-3 p-1 border'>
+            <label for="colorpicker">Color Picker:</label>
+            <input type="color" id="colorpicker" value={color} onChange={onChange} className='colorpicker' />
+            {/* <div class="input-group">
+              <span class="input-group-text">Color Picker</span>
+              <input type="color" class="form-control" id="colorpicker" value="#0000ff"/>
+              
+          </div> */}
+  
+          </div>
+  
+          <div className='col  d-flex justify-content-center m-3 p-1 border'>
+            <button type='subbit' className='btn btn-primary' onClick={onSubmit} disabled={!credit}> confirm</button>
+  
+          </div>
+  
+          <div className='col d-flex justify-content-center m-3 p-1 border'>
+  
+            <label for="credits">available pixels:</label>
+            <input type="button" id="credits" className='btn btn-outline-primary' value={credit} disabled />
+            {/* <p  value={credit}>{credit}</p> */}
+  
+          </div>
+  
+  
+        </div>
+  
+        </div>
+  
+      </>
+      
+    
     )
 }
 
